eeecd82db3e7fddf42b46dc9e0d6cb48fe0656a9
umarist <vahiktadevosyan@mail.ru>
2021-02-26 14:23:09 +0400

    homework26.03

--- beegraphy-model-editor-master/src/lesson24.02/lesson1.js
+++ beegraphy-model-editor-master/src/lesson24.02/lesson1.js
@@ -1,56 +1,102 @@
-import makerjs from "makerjs";
+import makerjs from "makerjs"
 
-const {Circle,Line,Arc} = makerjs.paths
-const {Rectangle, BezierCurve} = makerjs.models
-const {move, findSingleChain} = makerjs.model
-const {cloneToRow, childrenOnPath, childrenOnChain} = makerjs.layout
+const {BezierCurve, Rectangle} = makerjs.models;
+const {move, combineUnion} = makerjs.model;
+const {cloneToRow, childrenOnChain} = makerjs.layout;
+const {Arc, Line} = makerjs.paths;
+const {fillet} = makerjs.path;
+const {Holes, ConnectTheDots} = makerjs.models;
 
-export default function Model(general) {
+function Leaf(width, height, padding = 0) {
+    const round = height * 0.5;
+    const line1 = new Line([padding, 0], [0, height]);
+    const line2 = new Line([0, height], [width, height]);
+    const line3 = new Line([width, height], [width - padding, 0]);
+    const line4 = new Line([width - padding, 0], [padding, 0]);
+    const arc1 = fillet(line1, line4, round);
+    const arc2 = fillet(line3, line4, round);
+    this.paths = {line1, line2, line3, line4, arc1, arc2};
+}
 
-    const {side,height,round} = general
-    const width = 4 * (side * 0.3)
-    const padding = 2
+function OuterBox(side) {
+    const leafWidth = side + 3;
+    const leafHeight = 3 / 5 * side;
+    const leaf1 = new Leaf(leafWidth, leafHeight, Math.round(leafWidth * 0.01))
+    const leaf2 = new Leaf(leafWidth, leafHeight, Math.round(leafWidth * 0.12))
+    const leaf3 = new Leaf(leafWidth, leafHeight, Math.round(leafWidth * 0.01))
+    const leaf4 = new Leaf(leafWidth, leafHeight, Math.round(leafWidth * 0.12))
+    move(leaf1, [0, 0])
+    move(leaf2, [leafWidth, 0])
+    move(leaf3, [2 * leafWidth, 0])
+    move(leaf4, [3 * leafWidth, 0])
+    this.models = {
+        leaf1,
+        leaf2,
+        leaf3,
+        leaf4,
+    };
+}
 
-    const line1 = new Line([padding,0],[0,height])
-    const line2 = new Line([0,height],[side,height])
-    const line3 = new Line([side,height],[side - padding,0])
-    const line4 = new Line([side - padding,0],[padding,0])
+function ClosingPart(side, height){
 
-    const fill1 = makerjs.path.fillet(line1,line4,round)
-    const fill2 = makerjs.path.fillet(line3,line4,round)
-    const fill3 = makerjs.path.fillet(line1,line4,round)
-    const fill4 = makerjs.path.fillet(line3,line4,round)
+    const point1 = [0,0]
+    const point2 = [0,height]
+    const point3 = [side * 0.2,height]
+    const point4 = [side * 0.2,height * 0.08]
+    const closingPart = new ConnectTheDots(true,[point1,point2,point3,point4])
 
-    const fillModelFirst = {paths:{line1,line2,line3,line4,fill1,fill2}}
-    const fillModelFirstClone = makerjs.model.clone(fillModelFirst)
-    move(fillModelFirstClone,[2 * side, 0])
+    this.models = {closingPart}
+}
 
-    const fillModelSecond = {paths:{line1,line2,line3,line4,fill3,fill4}}
-    const fillModelSecondClone = makerjs.model.clone(fillModelFirst)
-    move(fillModelSecond, [side, 0])
-    move(fillModelSecondClone, [3 * side, 0])
+function Box(side,height) {
+    const leafHeight = 3 / 5 * side;
+    const leafWidth = side + 3
+    const boxPart = new Rectangle(leafWidth * 4,height)
+    move(boxPart,[0, leafHeight])
+    const closingPart = new ClosingPart(side, height)
+    move(closingPart,[leafWidth * 4, leafHeight])
+    const box = combineUnion(boxPart,closingPart)
 
+    this.models = {box}
 
+}
 
+function InnerBox(side, height) {
 
+    const firstPart = new Rectangle(side,side * 3)
+    const closingPart = new ClosingPart(side, height)
+    makerjs.model.rotate(closingPart,180)
+    move(closingPart,[0,height])
+    const closingPartMirror = makerjs.model.mirror(closingPart,true,false)
+    move(closingPartMirror,[side,height])
+    const combineParts = makerjs.model.combine(closingPart,closingPartMirror)
+    const combinePartsMirror = makerjs.model.mirror(combineParts,false, true)
+    move(combinePartsMirror, [0, side * 3])
+    const unionParts = combineUnion(combineParts,firstPart)
+    const secondPart = new Rectangle(side * 3, side)
+    move(secondPart,[-side,side])
+    const boxPart = combineUnion(combinePartsMirror, unionParts)
 
+    const innerBox = combineUnion(boxPart,secondPart)
+    move(innerBox,[side * 1.5,side * 2])
 
-
-    this.models = {fillModelFirst,fillModelSecond};
-    this.paths = {};
+    this.models = {innerBox}
 
 
 }
 
 
-Model.metaParameters =[
-    {
-        type: "group", title: "General",
-        parameters: [
-            {key: "side", title: "Side", type: "range", value: 30, min: 0, max: 50, step: 1},
-            {key: "height", title: "Height", type: "range", value: 30, min: 0, max: 50, step: 1},
-            {key: "round", title: "Round", type: "range", value: 1, min: 0, max: 50, step: 0.1},
-        ],
-    },
-];
 
+function PaperBox(side, height) {
+    const outerBox = new OuterBox(side, height)
+    const box = new Box(side,height)
+    const paperBox = combineUnion(outerBox,box)
+    const innerBox = new InnerBox(side, height)
+
+    this.models = {paperBox, innerBox}
+}
+PaperBox.metaParameters = [
+    {type: "range", title: "Width", min: 10, max: 500, value: 50},
+    {type: "range", title: "Height", min: 10, max: 500, value: 50},
+];
+export default PaperBox;
